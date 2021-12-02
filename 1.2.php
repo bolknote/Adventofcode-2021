f8e97c6ad41b0db618468e15560ca3496707bce0
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2021-12-02 14:01:52 +0300

    Task #1.2

--- 1.2.php
+++ 1.2.php
@@ -4,7 +4,9 @@ $r = file($_SERVER['argv'][1], FILE_IGNORE_NEW_LINES);
 $msg = [-1 => '(decreased)', '(unchanged)', '(increased)'];
 $prev = null;
 
-foreach ($r as $depth) {
+$cnt = count($r) - 2;
+for ($i = 0; $i < $cnt; $i++) {
+    $depth = array_sum(array_slice($r, $i, 3));
     echo $depth, ' ', $prev === null ? '(N/A - no previous measurement)' : $msg[$depth <=> $prev], "\n";
     $prev = $depth;
 }
