44d830916b448be1ef39f5f37ce3afde3177cd10
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2021-12-03 17:27:55 +0300

    Added current script path

--- day01/run.sh
+++ day01/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-code/part1.dc < input/input.txt | fgrep -c increased
-code/part2.php  input/input.txt | fgrep -c increased
+$(dirname "$0")/code/part1.dc < $(dirname "$0")/input/input.txt | fgrep -c increased
+$(dirname "$0")/code/part2.php  $(dirname "$0")/input/input.txt | fgrep -c increased
--- day02/run.sh
+++ day02/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-code/part1.rexx < input/input.txt
-code/part2.pl -- input/input.txt
+$(dirname "$0")/code/part1.rexx < $(dirname "$0")/input/input.txt
+$(dirname "$0")/code/part2.pl -- $(dirname "$0")/input/input.txt
