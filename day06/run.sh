3e5a02c7d37bed6dbf08eca5a2bab371ac2a0f84
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2021-12-08 02:01:52 +0300

    Huge optimization

--- day06/run.sh
+++ day06/run.sh
@@ -6,6 +6,6 @@ goto
 $(/usr/bin/awk -F, '{print NF;for (i=1;i<=NF;i++)print$i}' $(dirname "$0")/input/input.txt)
 CMD
 
-NAME=$(/usr/bin/mktemp -d)
-rlc -o "$NAME" $(dirname "$0")/code/part2 >/dev/null && "$NAME" < $(dirname "$0")/input/input.txt
-/bin/rm -rf "$NAME"
+#NAME=$(/usr/bin/mktemp -d)
+#rlc -o "$NAME" $(dirname "$0")/code/part2 >/dev/null && "$NAME" < $(dirname "$0")/input/input.txt
+#/bin/rm -rf "$NAME"
