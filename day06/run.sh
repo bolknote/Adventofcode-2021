eb40c82b9544f8c2f1fc8dd4f714979a97b6d372
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2021-12-07 07:13:50 +0300

    Day 6 part 1 solved

---
+++ day06/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+focal-81 <<CMD | /usr/bin/awk '{print $NF}'
+lib call $(dirname "$0")/code/part1.fcl
+goto
+$(/usr/bin/awk -F, '{print NF;for (i=1;i<=NF;i++)print$i}' $(dirname "$0")/input/input.txt)
+CMD
