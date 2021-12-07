14975348aab2a4b57cde950663bf52941f33c41a
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2021-12-08 01:26:06 +0300

    Added day 6 part 2

--- day06/run.sh
+++ day06/run.sh
@@ -5,3 +5,7 @@ lib call $(dirname "$0")/code/part1.fcl
 goto
 $(/usr/bin/awk -F, '{print NF;for (i=1;i<=NF;i++)print$i}' $(dirname "$0")/input/input.txt)
 CMD
+
+NAME=$(/usr/bin/mktemp -d)
+rlc -o "$NAME" $(dirname "$0")/code/part2 >/dev/null && "$NAME" < $(dirname "$0")/input/input.txt
+/bin/rm -rf "$NAME"
