b3e16cde3609f66bc5cc0f3836463c2f50eea8c5
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2021-12-10 01:56:56 +0300

    Run.sh

--- day08/run.sh
+++ day08/run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 $(dirname "$0")/code/part1.sh < $(dirname "$0")/input/input.txt
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+NAME=$(/usr/bin/mktemp -d)
+cd "$NAME"
+cp "$SCRIPT_DIR/code/part2.pas" .
+fpc part2.pas >/dev/null && ./part2 "$SCRIPT_DIR/input/input.txt"
+cd - >&-
+/bin/rm -rf "$NAME"
